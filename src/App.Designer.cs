be357b69a7cd7af201efb659426366fc2def1468
UNKNØWN <60184894+Unknxwn007@users.noreply.github.com>
2024-06-12 00:25:14 +0200

    alpha1
    not ready for use
    [TODO]
    Fix powershell code execution
    and sum i forgot

---
+++ src/App.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace src
+{
+    partial class App
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
+            button1 = new Button();
+            panel1 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            button3 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.Location = new Point(114, 227);
+            button1.Name = "button1";
+            button1.Size = new Size(159, 51);
+            button1.TabIndex = 0;
+            button1.Text = "Install Apps";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(24, 24, 24);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, 302);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(617, 38);
+            panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(224, 224, 224);
+            label1.Location = new Point(147, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(324, 19);
+            label1.TabIndex = 3;
+            label1.Text = "Developed and maintained by UNKNØWN";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = Starlight_Tool.Properties.Resources.icon_png;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(198, 26);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(207, 179);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(324, 227);
+            button3.Name = "button3";
+            button3.Size = new Size(159, 51);
+            button3.TabIndex = 4;
+            button3.Text = "Edit Applist";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // App
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(32, 32, 32);
+            ClientSize = new Size(617, 339);
+            Controls.Add(button3);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "App";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Starlight Tool";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Button button3;
+    }
+}
---
+++ src/Framework.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Management.Automation;
+using System.IO;
+
+namespace Starlight_Tool
+{
+    public class Framework
+    {
+        public static void ThemeManager(Form form, Control control, Control control2)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize");
+            object value = key.GetValue("AppsUseLightTheme");
+            bool isDarkModeEnabled = value != null && (int)value == 0;
+
+            if (isDarkModeEnabled)
+            {
+                form.BackColor = Color.FromArgb(32, 32, 32);
+                control.BackColor = Color.FromArgb(24, 24, 24);
+                control2.ForeColor = Color.FromArgb(200, 200, 200);
+            }
+            else
+            {
+                form.BackColor = Color.FromArgb(200, 200, 200);
+                control.BackColor = Color.FromArgb(175, 175, 175);
+                control2.ForeColor = Color.FromArgb(24, 24, 24);
+            }
+        }
+
+        public static void RunPSCode(string command)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    Arguments = command,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = false
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+        }
+
+        public static bool IsChocolateyInstalled()
+        {
+            return Directory.Exists(@"C:\ProgramData\chocolatey");
+        }
+
+        public static bool DoesPackagesFileExist()
+        {
+            return File.Exists("packages.config");
+        }
+
+        public static void RunChecks()
+        {
+            var installCommand = @"-ExecutionPolicy Bypass -Command ""iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))""";
+
+            //Chocolately 
+            if (!IsChocolateyInstalled())
+            {
+                MessageBox.Show("Chocolately has not been found! Press OK to install", "Requirements have not been met");
+                RunPSCode(installCommand);
+            }
+            //Files
+            if (!DoesPackagesFileExist())
+            {
+                //Download empty packages file
+            }
+        }
+    }
+}
